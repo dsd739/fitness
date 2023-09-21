c9868372a8e849d37febf945b24db8b4f23356e1
dsd739 <dsd739@gmail.com>
2023-09-21 22:30:00 +0530

    modified:   src/components/Feeds/Feeds.jsx

--- src/components/Feeds/Feeds.jsx
+++ src/components/Feeds/Feeds.jsx
@@ -5,21 +5,7 @@ import Scroller from "../Scroller/Scroller";
 const Feeds = () => {
   return (
     <>
-<<<<<<< HEAD
       <Scroller />
-=======
-    <div className='feeds'>
-        <div className="feedsnav"></div>
-        <div className="subfeeds">
-            <div className="post">
-
-            </div>
-            <div className="desciption">
-                <video src="https://youtu.be/tjF0LdL2Pvk?si=SBYDy9M7hFh0W-Dd"></video>
-            </div>
-        </div>
-    </div>
->>>>>>> 3e9d93b71c55d88498e1b37521c3f78d9e506824
     </>
   );
 };
