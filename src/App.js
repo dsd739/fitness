c89cdc71c06d17e670bfbe7643eeff999f10041c
Ace7568 <jayjoshi.2021cs@technonjr.org>
2023-09-21 12:20:01 +0530

    Changes to be committed: 	modified:   src/App.js 	modified:   src/components/Feeds/Feeds.css 	modified:   src/components/Feeds/Feeds.jsx

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Login from './components/Login/Login';
 function App() {
   return (
     <div className="App">
-      {/* <Login/>
-      <DashBoard/> */}
+      {/* <Login/> */}
+      <DashBoard/>
       <Feeds/>
     </div>
   );
