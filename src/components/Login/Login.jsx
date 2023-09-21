5256111c93b644491e6dbcf42c2dc5f5fd50fe4f
dsd739 <dsd739@gmail.com>
2023-09-21 11:44:19 +0530

    Changes to be committed: 	modified:   src/App.js 	modified:   src/components/Contest_Card/c_card.css 	modified:   src/components/Login/Login.jsx 	new file:   src/components/Login/login.css 	deleted:    src/components/Login/login.scss

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,161 +1,102 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
-import "./login.scss";
-
-export default function Login() {
-  const [isLoginFormVisible, setLoginFormVisible] = useState(true);
-=======
 import "./login.css";
 
 export default function Login() {
-  const [isLoginFormVisible, setLoginFormVisible] = useState(false);
->>>>>>> 7c51c7f14f8014964427ab51c6d32dfe19a30f3c
+  const [isSignUp, setIsSignUp] = useState(false);
 
-  const toggleForms = () => {
-    setLoginFormVisible(!isLoginFormVisible);
-  };
-<<<<<<< HEAD
-  const [userFormsClass, setUserFormsClass] = useState("");
-
-  // Function to handle "Sign Up" button click
   const handleSignUpClick = () => {
-    setUserFormsClass("bounceRight");
+    setIsSignUp(true);
   };
 
-  // Function to handle "Login" button click
-  const handleLoginClick = () => {
-    setUserFormsClass("bounceLeft");
+  const handleSignInClick = () => {
+    setIsSignUp(false);
   };
-=======
 
->>>>>>> 7c51c7f14f8014964427ab51c6d32dfe19a30f3c
   return (
-    <>
-      <section className="user">
-        <div className="user_options-container">
-          <div className="user_options-text">
-            <div className="user_options-unregistered">
-              <h2 className="user_unregistered-title">
-                Don't have an account?
-              </h2>
-              <p className="user_unregistered-text">
-                Banjo tote bag bicycle rights, High Life sartorial cray craft
-                beer whatever street art fap.
+    <div className="container-login">
+      <div className={`container ${isSignUp ? "right-panel-active" : ""}`}>
+        <div className="form-container sign-up-container">
+          <form action="#">
+            <h1>Create Account</h1>
+            <div className="social-container">
+              <a href="#" className="social">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="#" className="social">
+                <i className="fab fa-google-plus-g"></i>
+              </a>
+              <a href="#" className="social">
+                <i className="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+            <span>or use your email for registration</span>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button className="buttonn">Sign Up</button>
+          </form>
+        </div>
+        <div className="form-container sign-in-container">
+          <form action="#">
+            <h1>Sign in</h1>
+            <div className="social-container">
+              <a href="#" className="social">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="#" className="social">
+                <i className="fab fa-google-plus-g"></i>
+              </a>
+              <a href="#" className="social">
+                <i className="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+            <span>or use your account</span>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <a href="#">Forgot your password?</a>
+            <button className="buttonn">Sign In</button>
+          </form>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div
+              className={`overlay-panel overlay-left ${
+                isSignUp ? "" : "overlay-right"
+              }`}
+            >
+              <h1>{isSignUp ? "Hello, Friend!" : "Welcome Back!"}</h1>
+              <p>
+                {isSignUp
+                  ? "Enter your personal details and start the journey with us"
+                  : "To keep connected with us, please login with your personal info"}
               </p>
               <button
-                className="user_unregistered-signup"
-<<<<<<< HEAD
-                onClick={handleSignUpClick}
-=======
-                onClick={toggleForms}
->>>>>>> 7c51c7f14f8014964427ab51c6d32dfe19a30f3c
+                className="ghost buttonn"
+                onClick={isSignUp ? handleSignInClick : handleSignUpClick}
               >
-                Sign up
+                {isSignUp ? "Sign In" : "Sign Up"}
               </button>
             </div>
-
-            <div className="user_options-registered">
-              <h2 className="user_registered-title">Have an account?</h2>
-              <p className="user_registered-text">
-                Banjo tote bag bicycle rights, High Life sartorial cray craft
-                beer whatever street art fap.
-              </p>
-<<<<<<< HEAD
-              <button
-                className="user_registered-login"
-                onClick={handleLoginClick}
-              >
-=======
-              <button className="user_registered-login" onClick={toggleForms}>
->>>>>>> 7c51c7f14f8014964427ab51c6d32dfe19a30f3c
-                Login
-              </button>
-            </div>
-          </div>
-
-          <div
-            className={`user_options-forms ${
-              isLoginFormVisible ? "bounceLeft" : "bounceRight"
-            }`}
-          >
-            {isLoginFormVisible ? (
-              <div className="user_forms-login">
-                <h2 className="forms_title">Login</h2>
-                <form className="forms_form">
-                  <fieldset className="forms_fieldset">
-                    <div className="forms_field">
-                      <input
-                        type="email"
-                        placeholder="Email"
-                        className="forms_field-input"
-                        required
-                        autoFocus
-                      />
-                    </div>
-                    <div className="forms_field">
-                      <input
-                        type="password"
-                        placeholder="Password"
-                        className="forms_field-input"
-                        required
-                      />
-                    </div>
-                  </fieldset>
-                  <div className="forms_buttons">
-                    <button type="button" className="forms_buttons-forgot">
-                      Forgot password?
-                    </button>
-                    <input
-                      type="submit"
-                      value="Log In"
-                      className="forms_buttons-action"
-                    />
-                  </div>
-                </form>
-              </div>
-            ) : (
-              <div className="user_forms-signup">
-                <h2 className="forms_title">Sign Up</h2>
-                <form className="forms_form">
-                  <fieldset className="forms_fieldset">
-                    <div className="forms_field">
-                      <input
-                        type="text"
-                        placeholder="Full Name"
-                        className="forms_field-input"
-                        required
-                      />
-                    </div>
-                    <div className="forms_field">
-                      <input
-                        type="email"
-                        placeholder="Email"
-                        className="forms_field-input"
-                        required
-                      />
-                    </div>
-                    <div className="forms_field">
-                      <input
-                        type="password"
-                        placeholder="Password"
-                        className="forms_field-input"
-                        required
-                      />
-                    </div>
-                  </fieldset>
-                  <div className="forms_buttons">
-                    <input
-                      type="submit"
-                      value="Sign up"
-                      className="forms_buttons-action"
-                    />
-                  </div>
-                </form>
-              </div>
-            )}
           </div>
         </div>
-      </section>
-    </>
+      </div>
+
+      <footer>
+        <p>
+          Created with <i className="fa fa-heart"></i> by{" "}
+          <a target="_blank" href="https://florin-pop.com">
+            Florin Pop
+          </a>{" "}
+          - Read how I created this and how you can join the challenge{" "}
+          <a
+            target="_blank"
+            href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/"
+          >
+            here
+          </a>
+          .
+        </p>
+      </footer>
+    </div>
   );
 }
